ff184c1dc725eb77ac64e497f9a167a96b070dda
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-06-21 11:14:51 +0700

    s

---
+++ bai_tap/ReactJS_JSX/bai1/src/App.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const students = [
+  {
+    company: "Alfreds Futterkiste",
+    contact: "Maria Anders",
+    country: "Germany",
+  },
+  {
+    company: "Centro comercial Moctezuma",
+    contact: "Francisco Chang",
+    country: "Mexico",
+  },
+  {
+    company: "Ernst Handel",
+    contact: "Roland Mendel",
+    country: "Austria",
+  },
+  {
+    company: "Island Trading",
+    contact: "Helen Bennett",
+    country: "UK",
+  },
+  {
+    company: "Laughing Bacchus Winecellars",
+    contact: "Yoshi Tannamuri",
+    country: "Canada",
+  },
+  {
+    company: "Magazzini Alimentari Riuniti",
+    contact: "Giovanni Rovelli",
+    country: "Italy",
+  },
+];
+
+function App() {
+  return (
+    <>
+      <h1> Student Magnager </h1>{" "}
+      <table className="table table-striped ">
+        <thead>
+          <tr>
+            <th> Company </th> <th> Contact </th> <th> Country </th>{" "}
+          </tr>{" "}
+        </thead>{" "}
+        <tbody>
+          {" "}
+          {students.map((student) => (
+            <tr>
+              <td> {student.company} </td> <td> {student.contact} </td>{" "}
+              <td> {student.country} </td>{" "}
+            </tr>
+          ))}{" "}
+        </tbody>{" "}
+      </table>{" "}
+    </>
+  );
+}
+
+export default App;
